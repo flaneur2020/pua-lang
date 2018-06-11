daddbab06bb6e81cc1bbaa98b54b35513a6ae57c
tsuyoshiwada <wadackel@gmail.com>
2018-06-12 01:37:58 +0900

    feat: Implement `repl`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-mod token;
 mod lexer;
+mod repl;
+mod token;
+
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello! This is the Monkey programming language!");
+    println!("Feel free to type in commands");
+    println!("");
+    repl::start(io::stdin(), io::stdout());
 }
---
+++ src/repl.rs
@@ -0,0 +1,34 @@
+use lexer::Lexer;
+use std::io::{Stdin, Stdout, Write};
+use token::TokenType;
+
+pub fn start(stdin: Stdin, stdout: Stdout) {
+    loop {
+        let mut out = stdout.lock();
+
+        out.write(b">> ").unwrap();
+        out.flush().unwrap();
+
+        let mut line = String::new();
+
+        stdin.read_line(&mut line).expect("Failed to read line");
+
+        let mut l = Lexer::new(&line.trim());
+
+        loop {
+            let tok = l.next_token();
+
+            match tok.token_type {
+                TokenType::EOF => {
+                    break;
+                }
+                _ => {
+                    out.write(format!("{:?}\n", tok).as_bytes()).unwrap();
+                }
+            }
+        }
+
+        out.write(b"\n").unwrap();
+        out.flush().unwrap();
+    }
+}
